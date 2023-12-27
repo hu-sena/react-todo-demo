bc452d8b08353d6a35e8a4695b120a4d4505c9fc
Husna Zubir <shusnazubir@gmail.com>
2023-12-27 21:49:50 +0800

    delete item

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -23,13 +23,20 @@ function App() {
       rowNumber = 1;
     }
       const newTodo = {
-        rowNumber: todos.length + 1,
+        rowNumber: rowNumber,
         rowDescription: description,
         rowAssigned: assigned
       };
       setTodos(todos => [...todos, newTodo])
   }
   
+  const deleteTodo = (deleteTodoRowNumber) => {
+    // only return rows that is not deleted 
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  }
   return (
     <div className="mt-5 container">
       <div className='card'>
@@ -37,8 +44,8 @@ function App() {
           Your Todo's
         </div>
         <div className='card-body'>
-          <TodoTable todos={todos}/>
-          <button className='btn btn-primary' onClick={addTodo}>Add new todo</button>
+          <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+          <button className='btn btn-primary'>Add new todo</button>
           <NewTodoForm addTodo={addTodo}/>
         </div>
 
--- react-todos/src/components/TodoRowItem.js
+++ react-todos/src/components/TodoRowItem.js
@@ -1,7 +1,8 @@
 function TodoRowItem(props) {
     
     return (
-        <tr>
+        // delete row that is clicked
+        <tr onClick={() => props.deleteTodo(props.rowNumber)}>
             <th scope='row'>{props.rowNumber}</th>
             <td>{props.rowDescription}</td>
             <td>{props.rowAssigned}</td>
