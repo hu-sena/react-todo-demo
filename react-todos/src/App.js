ebe369cf2c154538eba635d284e2194acd747ab8
Husna Zubir <shusnazubir@gmail.com>
2023-12-27 20:09:38 +0800

    add form input

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
   
@@ -35,6 +36,7 @@ function App() {
         <div className='card-body'>
           <TodoTable todos={todos}/>
           <button className='btn btn-primary' onClick={addTodo}>Add new todo</button>
+          <NewTodoForm/>
         </div>
 
       </div>
