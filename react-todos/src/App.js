9c13cd6265680d3d29c9a57a7e3a97031dff6ac9
Husna Zubir <shusnazubir@gmail.com>
2023-12-27 19:22:18 +0800

    create react-springboot todo

---
+++ react-todos/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+import TodoTable from './components/TodoTable';
+
+function App() {
+  
+  const todos = [
+    {rowNumber: 1, rowDescription: 'Feed Puppy', rowAssigned: 'User 1'},
+    {rowNumber: 2, rowDescription: 'Water Plants', rowAssigned: 'User 2'},
+    {rowNumber: 3, rowDescription: 'Make Dinner', rowAssigned: 'User 1'}
+    
+  ]
+  
+  return (
+    <div className="mt-5 container">
+      <div className='card'>
+        <div className='card-header'>
+          Your Todo's
+        </div>
+        <div className='card-body'>
+          <TodoTable todos={todos}/>
+        </div>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
